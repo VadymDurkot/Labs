3f9318eb15f2cc61b08da3204ae277eb1b82eaea
VadymDurkot <57210295+VadymDurkot@users.noreply.github.com>
2021-01-11 21:59:17 +0200

    Add files via upload

---
+++ lab13/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\MENU.mdf;Integrated Security=True";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from Menu", ConnectionString);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+            connection.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(ConnectionString);
+            connection.Open();
+
+            string query = "INSERT INTO Menu (Name,Time,Price,Category)";
+            query += " VALUES (@Name,@Time,@Price,@Category)";
+
+            SqlCommand myCommand = new SqlCommand(query, connection);
+            myCommand.Parameters.AddWithValue("@Name", textBox1.Text);
+            myCommand.Parameters.AddWithValue("@Time", Convert.ToInt32(textBox2.Text));
+            myCommand.Parameters.AddWithValue("@Price", Convert.ToInt32(textBox3.Text));
+            myCommand.Parameters.AddWithValue("@Category", textBox4.Text);
+            // ... other parameters
+            myCommand.ExecuteNonQuery();
+
+
+            //command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(dataGridView1.CurrentCell.Value);
+            SqlConnection connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand("Delete from Menu where Id=@Id", connection);
+            command.Parameters.Add("@Id", id);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+
+                SqlConnection connection = new SqlConnection(ConnectionString);
+                connection.Open();
+                SqlCommand command = new SqlCommand("Update Menu Set Name=@Name,Time=@Time," +
+                    "Price=@Price,Category=@Category where Id=@Id", connection);
+                command.Parameters.Add("@Id", Convert.ToInt32(dataGridView1[0, i].Value));
+                //string s = dataGridView1[1, i].Value.ToString();
+                command.Parameters.Add("@Name", Convert.ToString(dataGridView1[1, i].Value));
+                command.Parameters.Add("@Time", Convert.ToInt32(dataGridView1[2, i].Value));
+                command.Parameters.Add("@Price", Convert.ToInt32(dataGridView1[3, i].Value));
+                command.Parameters.Add("@Category", Convert.ToString(dataGridView1[4, i].Value));
+                
+                connection.Close();
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            string query = "SELECT * " +
+                "FROM[Menu]" +
+                "Where[Menu].Price<10";
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            SqlCommand cmd = new SqlCommand(query, connection);
+            da.SelectCommand = cmd;
+            DataTable table = new DataTable();
+            da.Fill(table);
+            dataGridView1.DataSource = table;
+            connection.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            string query = "SELECT * " +
+                "FROM[Menu]" +
+                "Where[Menu].Price<5 AND Menu.Time<30" ;
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            SqlCommand cmd = new SqlCommand(query, connection);
+            da.SelectCommand = cmd;
+            DataTable table = new DataTable();
+            da.Fill(table);
+            dataGridView1.DataSource = table;
+            connection.Close();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            string query = "SELECT * " +
+                "FROM[Menu]" +
+                "Where Menu.Time = 30";
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            SqlCommand cmd = new SqlCommand(query, connection);
+            da.SelectCommand = cmd;
+            DataTable table = new DataTable();
+            da.Fill(table);
+            dataGridView1.DataSource = table;
+            connection.Close();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            string query = "SELECT * FROM Menu ORDER BY Price ";
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            SqlCommand cmd = new SqlCommand(query, connection);
+            da.SelectCommand = cmd;
+            DataTable table = new DataTable();
+            da.Fill(table);
+            dataGridView1.DataSource = table;
+            connection.Close();
+        }
+    }
+}
---
+++ lab13/WindowsFormsApp3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("WindowsFormsApp3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WindowsFormsApp3")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c8f068f9-c8cb-4643-80c8-67241e22b6e8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
